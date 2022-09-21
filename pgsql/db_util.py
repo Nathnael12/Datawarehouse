794276c0902e3c19b294ed28152001dc24ac7d5c
natty <natnaelmasresha@gmail.com>
2022-09-21 14:13:09 +0300

    fix insert  to table method

--- pgsql/db_util.py
+++ pgsql/db_util.py
@@ -1,32 +1,45 @@
 import pandas as pd
 from sqlalchemy import text
-
+import json
 from sqlalchemy import create_engine
+
 engine = create_engine('postgresql+psycopg2://postgres:root@host.docker.internal/traffic_data')
 
-VEHICLE_SCHEMA = "timed=vehicle_data_schema.sql"
+VEHICLE_SCHEMA = "timed_vehicle_data_schema.sql"
 TRAJECTORIES_SCHEMA = "trajectory_schema.sql"
 
 
 def create_table():
     try:
         with engine.connect() as conn:
-            for name in [VEHICLE_SCHEMA, TRAJECTORIES_SCHEMA]:
-                with open(f'{name}') as file:
+            for name in [TRAJECTORIES_SCHEMA,VEHICLE_SCHEMA]:
+                
+                with open(f'/opt/pgsql/{name}', "r") as file:
                     query = text(file.read())
                     conn.execute(query)
         print("Successfull")
     except Exception as e:
-        print(e)
+        print("Error creating table",e)
 
 
 # create_table()
 
-def insert_table(df: pd.DataFrame, table_name: str ):
+def insert_to_table(json_stream :str, table_name: str,from_file=False ):
     try:
+        if not from_file:
+            df = pd.read_json(json_stream)
+        else:
+            # df = pd.read_json(f'../temp/{json_stream}')
+            with open(f'../temp_storage/{json_stream}','r') as file:
+                data=file.readlines()
+            dt=data[0]
+
+            df=pd.DataFrame.from_dict(json.loads(dt))
+            
         with engine.connect() as conn:
-            df.to_sql(name=table_name, con=conn, if_exists='replace', index=False)
+            df.to_sql(name=table_name, con=conn, if_exists='append', index=False)
+
     except Exception as e:
-        print(f"Failed  ---- {e}")  
+        print(f"error insert to table: {e}")  
 
 
