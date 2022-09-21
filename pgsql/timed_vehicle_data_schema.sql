5b54fea980d3142e30352ec134baa30adfacac47
natty <natnaelmasresha@gmail.com>
2022-09-21 16:45:00 +0300

    make dtype changes in db schema

--- pgsql/timed_vehicle_data_schema.sql
+++ pgsql/timed_vehicle_data_schema.sql
@@ -2,12 +2,12 @@ CREATE TABLE IF NOT EXISTS vehicles
 (
     "id" SERIAL NOT NULL,
     "track_id" TEXT NOT NULL,
-    "lat" FLOAT NOT NULL,
-    "lon" FLOAT DEFAULT NULL,
-    "speed" FLOAT DEFAULT NULL,
-    "lon_acc" FLOAT DEFAULT NULL,
-    "lat_acc" FLOAT DEFAULT NULL,
-    "time" FLOAT DEFAULT NULL,
+    "lat" TEXT NOT NULL,
+    "lon" TEXT DEFAULT NULL,
+    "speed" TEXT DEFAULT NULL,
+    "lon_acc" TEXT DEFAULT NULL,
+    "lat_acc" TEXT DEFAULT NULL,
+    "time" TEXT DEFAULT NULL,
     PRIMARY KEY ("id"),
     CONSTRAINT fk_trajectory
         FOREIGN KEY("track_id") 
--- pgsql/trajectory_schema.sql
+++ pgsql/trajectory_schema.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS trajectories
 (
     "track_id" TEXT NOT NULL,
     "type" TEXT NOT NULL,
-    "traveled_d" FLOAT DEFAULT NULL,
-    "avg_speed" FLOAT DEFAULT NULL,
+    "traveled_d" TEXT DEFAULT NULL,
+    "avg_speed" TEXT DEFAULT NULL,
     -- "track_id" INT DEFAULT NULL,
     PRIMARY KEY ("track_id")
 );
