c2e2b4b690abd9a334d87c27322618b65f27a8b7
natty <natnaelmasresha@gmail.com>
2022-09-22 11:39:44 +0300

    applied dbt successfully

---
+++ dbt/traffic_data/models/example/ten_vehicles.sql
@@ -0,0 +1,24 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='view') }}
+
+with source_data as (
+
+    select * from vehicles limit 10
+)
+
+select *
+from source_data
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
