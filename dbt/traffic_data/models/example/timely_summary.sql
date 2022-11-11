6c6d77566e46b633c73163e3acf39a8ec02c8ff2
natty <natnaelmasresha@gmail.com>
2022-11-11 10:17:28 +0300

    add a summary of speeds group by time

---
+++ dbt/traffic_data/models/example/timely_summary.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='view') }}
+
+with timely_summary as (
+    SELECT 
+    time,
+    Round(AVG(Cast(speed as numeric)),2) as "speed",
+    Round(AVG(Cast(lat_acc as numeric)),2) as "lat_acc",
+    Round(AVG(Cast(lon_acc as numeric)),2) as "lon_acc"
+    from vehicles
+    GROUP BY "time"
+)
+
+
+SELECT * FROM timely_summary
+    
